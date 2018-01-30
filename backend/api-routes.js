a251e82145a2026aa3ffc23c2c89ae7b890a5c57
OpaL2 <opal@palojarvi.fi>
2018-01-30 22:56:16 +0200

    Addded ok and error response functions
    Now has meaningful way to response api requests, first endpoint of api should now be in working condition.

--- backend/api-routes.js
+++ backend/api-routes.js
@@ -1,23 +1,35 @@
 'use strict';
 
 var express = require('express');
+var util = require('util')
 
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
 var router = express.Router();
 
+var log = require('./log');
+var locations = require('./locations')
 
+//responsible for sending responses
 function ok(res) {
+  return (data) => {
+    res.json(data);
+  };
+};
 
-}
-
-function err(res) {
-
-}
+//Handles all api internal server error situations
+function err(res, errStatus) {
+  if(errStatus === undefined) 
+    {errStatus = 500};
+  return (data) => {
+    log.error('Caught error', util.inspect(data));
+    res.sendStatus(errStatus):
+  };
+};
 
 router.get('/locations', (req, res) => {
-
+  locations.getAll().then(ok(res), err(res))
 });
 
 router.get('/measurements/latest', (req, res) => {
