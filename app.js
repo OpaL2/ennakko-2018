e626dae2ffc62471ecb8571aeff69c9b24d9de92
OpaL <opal@palojarvi.fi>
2018-02-02 20:46:30 +0200

    Add script and style paths to app and ask them from index.html

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ var config = require('./config/config.js');
 app.use('/api', apiRoutes);
 
 app.get(['/', '/index.html'], (req, res) => {
-  res.sendFile(__dirname + '/frontend/index.html');
+  res.sendFile(path.join(__dirname,'/frontend/index.html'));
 });
+
+app.use('/public/', express.static(path.join(__dirname, '/frontend/public')));
 
 app.listen(config.port, () => {
   log.info('App is listening port: ' + config.port);
