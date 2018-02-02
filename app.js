6219f28ede10a55cc1b02aee1c3132a4f9f41885
OpaL <opal@palojarvi.fi>
2018-02-02 20:56:52 +0200

    Add frontend js foundary to index.html and app.js files.

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var config = require('./config/config.js');
 app.use('/api', apiRoutes);
 
 app.get(['/', '/index.html'], (req, res) => {
-  res.sendFile(path.join(__dirname,'/frontend/index.html'));
+  res.sendFile(__dirname +'/frontend/index.html');
 });
 
-app.use('/public/', express.static(path.join(__dirname, '/frontend/public')));
+app.use('/public/', express.static(__dirname + '/frontend/public'));
 
 app.listen(config.port, () => {
   log.info('App is listening port: ' + config.port);
