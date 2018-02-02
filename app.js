c7c18f64bb15cc8fea79fed367753058367270f6
OpaL <opal@palojarvi.fi>
2018-02-02 20:33:46 +0200

    Move app.js to root directory and modify files as needed. Add index.html for default page.

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var log = require('./backend/log');
+var apiRoutes = require('./backend/api-routes');
+var config = require('./config/config.js');
+
+app.use('/api', apiRoutes);
+
+app.get(['/', '/index.html'], (req, res) => {
+  res.sendFile(__dirname + '/frontend/index.html');
+});
+
+app.listen(config.port, () => {
+  log.info('App is listening port: ' + config.port);
+});
