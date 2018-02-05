351b6ed39bcb19fd98fc212ac860f57edfbce03e
OpaL <opal@palojarvi.fi>
2018-02-05 09:49:05 +0200

    Add react and jQuery for frontend, create build enviroment for supporting es6, browserify and react

---
+++ frontend/js/App.jsx
@@ -0,0 +1,6 @@
+//JS file for frontend
+
+var $ = require('jquery');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,54 @@
 'use strict';
 
 var gulp = require('gulp');
-var cssmin = require('gulp-css-clean');
+var cssmin = require('gulp-clean-css');
 var jsmin = require('gulp-uglify');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var server = require('gulp-live-server');
+var rename = require('gulp-rename');
+var babel = require('gulp-babel');
+var browserify = require('gulp-browserify');
+
+gulp.task('sass', () => {
+  return gulp.src('./frontend/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(cssmin())
+    .pipe( rename ( (path) => {
+      path.extname = '.min.css';
+    }))
+    .pipe(gulp.dest('./frontend/public/css/'));
+});
+
+gulp.task('sass:watch', () => {
+  gulp.watch('frontend/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('js:minify', () => {
+  return gulp.src('./frontend/js/App.jsx')
+    .pipe(browserify({
+      insertGlobals: true,
+      debug: true
+    }))
+    .pipe(babel({
+      presets: ['env','react']
+    }))
+    .pipe(jsmin())
+    .pipe(rename( (path) => {
+      path.extname = '.min.js';
+    }))
+    .pipe(gulp.dest('./frontend/public/js/'));
+});
+
+gulp.task('js:minify:watch', () => {
+  gulp.watch('frontend/js/**/*.js*', ['js:minify']);
+});
+
+gulp.task('watch', ['js:minify:watch', 'sass:watch']);
+
+gulp.task('server', () => {
+  var app = server.new('app.js');
+  app.start();
+});
+
+gulp.task('default', ['server', 'watch']);
